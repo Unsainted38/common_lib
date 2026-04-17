e3ffdd98a02b9a2da68431dc801bcb05c265ddd0
Unsainted38 <erik.veraksich@yandex.ru>
2026-04-17 15:09:02 +0300

    Rewritten Abstract command interface

--- cmd/abstract_command.cpp
+++ cmd/abstract_command.cpp
@@ -1,12 +1,40 @@
 #include "abstract_command.h"
 
-AbstractCommand::AbstractCommand(ValueType value_type, CommandType cmd_type, QObject *parent)
-    :   QObject(parent),
-        m_type(value_type),
-        cmdType(cmd_type) {
+AbstractCommand::AbstractCommand(QObject *parent)
+    :   QObject(parent) {
 
 }
 
+QVariant AbstractCommand::getValue()
+{
+    qDebug() << "[WARNING!!!] In AbstractCommand class getValue() [WARNING!!!]";
+}
+
+void AbstractCommand::setValue(QVariant v)
+{
+    Q_UNUSED(v);
+    qDebug() << "[WARNING!!!] In AbstractCommand class setValue() [WARNING!!!]";
+}
+
+bool AbstractCommand::isSuccess()
+{
+    qDebug() << "[WARNING!!!] In AbstractCommand class isSuccess() [WARNING!!!]";
+}
+
 void AbstractCommand::processData(const QByteArray &data, quint16 regAddr) {
+    Q_UNUSED(data);
+    Q_UNUSED(regAddr);
     qDebug() << "[WARNING!!!] In AbstractCommand class processData() [WARNING!!!]";
+}
+
+void AbstractCommand::processData(const QByteArray &data)
+{
+    Q_UNUSED(data);
+    qDebug() << "[WARNING!!!] In AbstractCommand class processData() [WARNING!!!]";
+}
+
+bool AbstractCommand::tryParse(const QByteArray &data)
+{
+    Q_UNUSED(data);
+    qDebug() << "[WARNING!!!] In AbstractCommand class tryParse() [WARNING!!!]";
 }
--- cmd/abstract_command.h
+++ cmd/abstract_command.h
@@ -6,6 +6,7 @@
 #include <QDataStream>
 #include <QDebug>
 #include <QList>
+#include <QVariant>
 
 enum ValueType {
     QINT8 = 7,
@@ -30,114 +31,20 @@ enum class CommandType {
 class AbstractCommand : public QObject {
     Q_OBJECT
 public:
-    explicit AbstractCommand(ValueType value_type, CommandType cmd_type = CommandType::READWRITE, QObject *parent = nullptr);
+    explicit AbstractCommand(QObject *parent = nullptr);
     virtual const QByteArray &makeCommand() = 0;
-    template <typename T> T getV() {
-        switch(m_type) {
-            case ValueType::QINT8:
-                return static_cast<T>(m_val8);
 
-            case ValueType::QUINT8:
-                return static_cast<T>(m_valu8);
+    virtual QVariant getValue();
+    virtual void setValue(QVariant v);
+    virtual bool isSuccess();
 
-            case ValueType::QUINT16:
-                return static_cast<T>(m_valu16);
+    virtual bool tryParse(const QByteArray &data);
 
-            case ValueType::QUINT32:
-                return static_cast<T>(m_valu32);
-
-            case ValueType::QINT32:
-                return static_cast<T>(m_val32);
-
-            case ValueType::FLOAT:
-                return static_cast<T>(m_valf);
-
-            case ValueType::DOUBLE:
-                return static_cast<T>(m_vald);
-        }
-    }
-    template<typename T> void setV(const T &v) {
-        switch(m_type) {
-            case ValueType::QINT8:
-                m_val8 = (qint8)v;
-                break;
-
-            case ValueType::QUINT8:
-                m_valu8 = (quint8)v;
-                break;
-
-            case ValueType::QUINT16:
-                m_valu16 = (quint16)v;
-                break;
-
-            case ValueType::QUINT32:
-                m_valu32 = (quint32)v;
-                break;
-
-            case ValueType::QINT32:
-                m_val32 = (qint32)v;
-                break;
-
-            case ValueType::FLOAT:
-                m_valf = (float)v;
-                break;
-
-            case ValueType::DOUBLE:
-                m_vald = (double)v;
-                break;
-        }
-    }
-    void setS(QString s) {
-        m_valQStr = s;
-    }
-    QString getS() {
-        return m_valQStr;
-    }
-    void setByteArray(QByteArray ba) {
-        m_valQByteArray = ba;
-    }
-    QByteArray getByteArray() {
-        return m_valQByteArray;
-    }
-
-    void setListQuint16(QList<quint16> list) {
-        m_valListQuint16 = list;
-    }
-    QList<quint16> getListQuint16() {
-        return m_valListQuint16;
-    }
-    void setListQuint8(QList<quint8> list) {
-        m_valListQuint8 = list;
-    }
-    QList<quint8> getListQuint8() {
-        return m_valListQuint8;
-    }
-
-    void setBool(bool b) {
-        m_valBool = b;
-    }
-
-    bool getBool() {
-        return m_valBool;
-    }
 public slots:
     virtual void processData(const QByteArray &data, quint16 regAddr);
+    virtual void processData(const QByteArray &data);
 signals:
 protected:
-    ValueType m_type;
-    CommandType cmdType;
-    qint8 m_val8 = 0;
-    quint8 m_valu8 = 0;
-    quint16 m_valu16 = 0;
-    quint32 m_valu32 = 0;
-    qint32 m_val32 = 0;
-    QString m_valQStr = "";
-    QByteArray m_valQByteArray = "";
-    QList<quint16> m_valListQuint16;
-    QList<quint8> m_valListQuint8;
-    float m_valf = 0.0;
-    double m_vald = 0.0;
-    bool m_valBool = false;
 };
 
 #endif // ABSTRACTCOMMAND_H
